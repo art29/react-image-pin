9003059f1a428956e3a234f83e352d818668d93e
Arthur Fetiveau <art.fetiveau@gmail.com>
2024-02-18 16:56:45 -0500

    fix: Library Missing Config

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"], insertTypesEntry: true })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ["lib"], insertTypesEntry: true }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, "lib/main.ts"),
-      name: "React Image Pin",
-      formats: ["es", "umd"],
-      fileName: "ui-kit",
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime", "tailwindcss"],
