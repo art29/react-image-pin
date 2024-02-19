6cab0d5ddec84b27a16c1f7b7fb426b84fb14feb
Arthur Fetiveau <art.fetiveau@gmail.com>
2024-02-19 16:38:55 -0500

    fix: added back css

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"], insertTypesEntry: true })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ["lib"], insertTypesEntry: true }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
